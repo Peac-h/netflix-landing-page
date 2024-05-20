d13aaec841d2daa281f29a144cfae34c8cf695e7
_Peach <p6eac.h@gmail.com>
2024-05-20 22:24:50 +0400

    Make feature adjustemnts

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,21 +1,51 @@
 import { ReactNode } from "react";
 import "./Features.scss";
-import { featuresInfo } from "../pageInfo";
 
 export function Features() {
   return (
     <div className="features">
-      {featuresInfo.map((feature, index) => (
-        <Feature
-          key={index}
-          header={feature.header}
-          paragraph={feature.paragraph}
-          imageSrc={feature.imageSrc}
-          videoSrc={feature.videoSrc}
-          isFirstVideo={index === 0}
-          reverse={index % 2 !== 0}
-        />
-      ))}
+      <Feature
+        header={`Enjoy on your TV`}
+        paragraph={`Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.`}
+        imageSrc={`https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png`}
+        videoSrc={`https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v`}
+        isFirstVideo
+      />
+
+      <Feature
+        header={`Download your shows to watch offline`}
+        paragraph={`Save your favorites easily and always have something to watch.`}
+        imageSrc={`https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg`}
+        reverse
+      >
+        <div className="feature-media-box">
+          <div className="feature-media-image-box">
+            <img
+              alt=""
+              src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/boxshot.png"
+            />
+          </div>
+          <div className="feature-media-text-box">
+            <div className="feature-media-text-title">Stranger Things</div>
+            <div className="feature-media-text-status">Downloading...</div>
+          </div>
+          <div className="feature-media-animation-box"></div>
+        </div>
+      </Feature>
+
+      <Feature
+        header={`Watch everywhere`}
+        paragraph={`Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.`}
+        imageSrc={`https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile.png`}
+        videoSrc={`https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-devices.m4v`}
+      />
+
+      <Feature
+        header={`Create profiles for kids`}
+        paragraph={`Send kids on adventures with their favorite characters in a space made just for them—free with your membership.`}
+        imageSrc={`https://occ-0-7681-3467.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABejKYujIIDQciqmGJJ8BtXkYKKTi5jiqexltvN1YmvXYIfX8B9CYwooUSIzOKneblRFthZAFsYLMgKMyNfeHwk16DmEkpIIcb6A3.png?r=f55`}
+        reverse
+      />
     </div>
   );
 }
